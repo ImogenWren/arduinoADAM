174cce05e68a82ca3aef684c556fc1decef93b22
ImogenWren <imogen.wren91@gmail.com>
2023-10-12 11:07:30 +0100

    got basic modbus working

--- adamModbus.py
+++ adamModbus.py
@@ -5,53 +5,69 @@ adamModbus
 - Attempt to move away from ASCII commands and towards a more robust implementation using MODBUS
 
 https://control.com/forums/threads/reading-di-do-status-of-adam-6050.27164/
+
+import pymodbus
+import minimalmodbus
+
+Example: Force Coil 3 (Address 00003) to ON in an ADAM-6000 module.
+01 05 00 03 FF 00
+
+
+Example: Force Coil DO 0 (Address 00017 (0x11)) to ON in an ADAM-6000 module.
+01 05 00 11 FF 00
+
+# READ THE DOCS!
+https://pypi.org/project/pyModbusTCP/
 '''
+# Ethernet Delarations
+ADAM_6052_IP = "192.168.1.111"
+ADAM_6217_IP = "192.168.1.112"
+#PORT = 1025    #1024-1029 valid for http
+PORT = 502
+BUFFER = 1024
 
-#import pymodbus
-#import minimalmodbus
 
+DO_0 = 0x10  # THIS MAKES NO SENSE!! should be 0x11 given documentation!
+DO_1 = 0x11
+DO_2 = 0x12
+DO_3 = 0x13
+DO_4 = 0x14
+DO_5 = 0x15
+DO_6 = 0x16
+DO_7 = 0x17
 
-from pymodbus.client import ModbusTcpClient
+DO_list = [DO_0,DO_1,DO_2,DO_3,DO_4,DO_5,DO_6,DO_7]
+
+## Init
+from pyModbusTCP.client import ModbusClient
+import time
 
 # Create a Modbus TCP client
-client = ModbusTcpClient('192.168.1.111')  # Replace with your device's IP address
-
-# Connect to the Modbus TCP server
-client.connect()
-
-
-# Perform Modbus operations here
-from pymodbus.constants import Endian
-from pymodbus.payload import BinaryPayloadDecoder
-from pymodbus.payload import BinaryPayloadBuilder
+#Module init (TCP always open)
+adam6052 = ModbusClient(host=ADAM_6052_IP,port=PORT, unit_id=1, auto_open=True)
+bit = True
 
 
-
-# Build a command  -- UNTESTED FODUND IN RANDOM DOCS
-#https://github.com/eterey/pymodbus3/blob/master/examples/common/modbus-payload.py
-
-builder = BinaryPayloadBuilder(endian=Endian.Little)
-builder.add_string('abcdefgh')
-builder.add_32bit_float(22.34)
-builder.add_16bit_uint(0x1234)
-builder.add_8bit_int(0x12)
-builder.add_bits([0,1,0,1,1,0,1,0])
-payload = builder.build()
-address = 0x01
-result  = client.write_registers(address, payload, skip_encode=True)
-
-
-# Read a holding register value
-result = client.read_holding_registers(address=301, count=1, unit=1)
-if result.isError():
-    print("Error reading register!")
-else:
-    decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big)
-    value = decoder.decode_32bit_float()
-
-    print("Register value:", value)
+def adam6052_set_coil(DO_number, state):
+    no_error = adam6052.write_single_coil(DO_list[DO_number], state)
+    # no_error = client.w
+    time.sleep(0.5)
+    if (no_error):
+        print(f"adam6052: coil DO_{DO_number}: Written to {state}")
+        return 0   # return false if complete
+    else:
+        print(f"adam6052: coil DO_{DO_number}: Unable to write to {state}")
+        return 1  # error code can be returned here
 
 
 
-# Close the connection
-client.close()
+## Main Loop
+while True:
+    print("Starting Modbus Connection")
+    adam6052_set_coil(0, True)
+    time.sleep(4)
+    adam6052_set_coil(0, False)
+    time.sleep(4)
+
+
+
