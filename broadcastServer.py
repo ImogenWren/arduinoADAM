f8f16ba542daa7016fb208f67156c3b179afb255
ImogenWren <imogen.wren91@gmail.com>
2023-11-23 11:49:06 +0000

    sdf

--- broadcastServer.py
+++ broadcastServer.py
@@ -21,27 +21,30 @@ PORT = 65432
 TEST_COMMAND = '{"cmd":"set", "V1":"open"}'
 
 
-
-print(f"Starting Broadcast Server:\nListening on {HOST}:{PORT}")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    while (1):
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            iteration = 0
-            while True:
-                print(iteration)
-                json_command = TEST_COMMAND.encode("UTF-8")
-                conn.sendall(json_command)
-                data = conn.recv(2048)
-                data_dic = pack.unpack_json(data)
-                json_print = pack.dump_json()
-                #print(data_dic)
-                if not data:
-                    break
-                iteration += 1
-                time.sleep(1)
+while(1):
+    try:
+        print(f"Starting Broadcast Server:\nListening on {HOST}:{PORT}")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            while (1):
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print(f"Connected by {addr}")
+                    iteration = 0
+                    while True:
+                        print(iteration)
+                        json_command = TEST_COMMAND.encode("UTF-8")
+                        conn.sendall(json_command)
+                        data = conn.recv(2048)
+                        data_dic = pack.unpack_json(data)
+                        json_print = pack.dump_json()
+                        #print(data_dic)
+                        if not data:
+                            break
+                        iteration += 1
+                        time.sleep(1)
+    except:
+        print("Exception Handled, restarting")
 
 
--- websocketClient.py
+++ websocketClient.py
@@ -4,17 +4,24 @@ import socket
 import time
 import acUnitGlobals as glbs
 
+
 pack = glbs.jsonPack
+
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 TESTHOST = "10.42.0.1"
 PORT = 65432  # The port used by the server
 
+json_delay = 1   ## time between json messages to server
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((TESTHOST, PORT))
+    init_time = time.time()
     while(1):
-        json_message = pack.dump_json()
-        s.sendall(json_message.encode("UTF-8"))
+        if time.time() - init_time >= json_delay:
+            json_message = pack.dump_json()
+            s.sendall(json_message.encode("UTF-8"))
         data = s.recv(1024)
         print(f"Received {data!r}")
-        time.sleep(5)
+        #time.sleep(5)
